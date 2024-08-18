f23a5fbf00fcef69d41488a654f9369739a5bd11
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-18 01:04:28 -0700

    port old home menu code to new hierarchy

--- scenes/home.c
+++ scenes/home.c
@@ -1,1 +1,55 @@
 #include "../sensors.h"
+
+static void home_callback(void* ctx, uint32_t index) {
+    SensorsApp* s = ctx;
+    switch(index) {
+    case SensorsAppMenu_SelectDummySensor:
+        scene_manager_handle_custom_event(s->sm, SensorsAppEvent_UseDummySensor);
+        break;
+    case SensorsAppMenu_SelectBH1750:
+        scene_manager_handle_custom_event(s->sm, SensorsAppEvent_UseBH1750);
+        break;
+    default:
+        break;
+    }
+}
+
+void sensors_scene_home_on_enter(void* ctx) {
+    SensorsApp* s = ctx;
+    submenu_reset(s->main_menu);
+    submenu_add_item(
+        s->main_menu, "Dummy Sensor", SensorsAppMenu_SelectDummySensor, home_callback, s);
+    submenu_add_item(s->main_menu, "BH1750", SensorsAppMenu_SelectBH1750, home_callback, s);
+    view_dispatcher_switch_to_view(s->vd, SensorsAppView_Menu);
+}
+
+bool sensors_scene_home_on_event(void* ctx, SceneManagerEvent evt) {
+    SensorsApp* s = ctx;
+    bool consumed = false;
+    switch(evt.type) {
+    case SceneManagerEventTypeCustom:
+        switch(evt.event) {
+        case SensorsAppEvent_UseDummySensor:
+            scene_manager_next_scene(s->sm, SensorsAppScene_DummySensor);
+            consumed = true;
+            break;
+        case SensorsAppEvent_UseBH1750:
+            scene_manager_next_scene(s->sm, SensorsAppScene_BH1750);
+            consumed = true;
+            break;
+        default:
+            consumed = false;
+            break;
+        }
+        break;
+    default: // eg. SceneManagerEventTypeBack, SceneManagerEventTypeTick
+        consumed = false;
+        break;
+    }
+    return consumed;
+}
+
+void sensors_scene_home_on_exit(void* ctx) {
+    SensorsApp* s = ctx;
+    submenu_reset(s->main_menu);
+}
--- sensors.c
+++ sensors.c
@@ -5,26 +5,34 @@ bool navigation_event_handler(void* ctx) {
     return scene_manager_handle_back_event(s->sm);
 }
 
+bool custom_event_handler(void* ctx, uint32_t custom_event) {
+    SensorsApp* s = ctx;
+    return scene_manager_handle_custom_event(s->sm, custom_event);
+}
+
+// init scene manager
 void sm_init(SensorsApp* s) {
-    // init scene manager with list of all handlers
+    // allocate memory for scene manager
     s->sm = scene_manager_alloc(&handlers, s);
 }
 
+// init view dispatcher
 void vd_init(SensorsApp* s) {
-    // init view dispatcher
+    // allocate memory for view dispatcher
     s->vd = view_dispatcher_alloc();
     view_dispatcher_enable_queue(s->vd);
 
     // allocate views
-    s->menu = submenu_alloc();
+    s->main_menu = submenu_alloc();
     s->sensor_config = variable_item_list_alloc();
 
     // assign callback to pass events from views to scene manager
     view_dispatcher_set_event_callback_context(s->vd, s);
-    // view_dispatcher_set_custom_event_callback(s->vd, ViewDispatcherCustomEventCallback callback)
+    view_dispatcher_set_custom_event_callback(s->vd, custom_event_handler);
     view_dispatcher_set_navigation_event_callback(s->vd, navigation_event_handler);
 
     // add views to dispatcher
+    view_dispatcher_add_view(s->vd, SensorsAppView_Menu, submenu_get_view(s->main_menu));
     view_dispatcher_add_view(
         s->vd, SensorsAppView_SensorConfig, variable_item_list_get_view(s->sensor_config));
 }
@@ -43,7 +51,7 @@ void sensors_app_free(SensorsApp* s) {
     view_dispatcher_remove_view(s->vd, SensorsAppView_Menu);
     view_dispatcher_remove_view(s->vd, SensorsAppView_SensorConfig);
     view_dispatcher_free(s->vd);
-    submenu_free(s->menu);
+    submenu_free(s->main_menu);
     variable_item_list_free(s->sensor_config);
     free(s);
 }
@@ -61,7 +69,7 @@ int32_t sensors_app(void* p) {
     // attach dispatcher to gui
     view_dispatcher_attach_to_gui(s->vd, gui, ViewDispatcherTypeFullscreen);
     // set next scene and begin general logic
-    scene_manager_next_scene(s->sm, SensorsAppScene_MainMenu);
+    scene_manager_next_scene(s->sm, SensorsAppScene_Home);
     // start view dispatcher
     view_dispatcher_run(s->vd);
 
