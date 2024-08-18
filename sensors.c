63c330a1790cbf1d799d836042f1d356ddbfc81e
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-17 21:30:02 -0700

    add entrypoint code

--- sensors.c
+++ sensors.c
@@ -36,11 +36,28 @@ void sensors_app_free(SensorsApp* s) {
     free(s);
 }
 
-// main app logic
+// entrypoint
 int32_t sensors_app(void* p) {
     UNUSED(p);
     FURI_LOG_I("TEST", "Hello world");
     FURI_LOG_I("TEST", "I'm sensors!");
+
+    // begin main app init
+    SensorsApp* s = sensors_app_init();
+
+    // set first scene of app
+    // create gui object
+    Gui* gui = furi_record_open(RECORD_GUI);
+    // attach dispatcher to gui
+    view_dispatcher_attach_to_gui(s->vd, gui, ViewDispatcherTypeFullscreen);
+    // set next scene and begin general logic
+    scene_manager_next_scene(s->sm, SensorsAppScene_MainMenu);
+    // start view dispatcher
+    view_dispatcher_run(s->vd);
+
+    // once at this point, begin cleanup
+    furi_record_close(RECORD_GUI);
+    sensors_app_free(s);
 
     return 0;
 }
