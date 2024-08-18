b97660f6b2edd6411b701bc7ef12a3ca5e390c5a
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-18 03:31:43 -0700

    bit shift address, properly choose how many bytes to tx/rx, and set up doxygen

--- util/i2c_trx.c
+++ util/i2c_trx.c
@@ -13,24 +13,30 @@ void i2c_free(I2CTRx* i) {
     free(i);
 }
 
+/**
+ * @brief 
+ * @details
+ * @param i i2c context
+ */
 void i2c_tx(I2CTRx* i) {
     // wipe rx data to prepare for rx
     memset(i->rx, 0, 256);
-    // dynamically allocate array for expected bytes
-    uint8_t* rx_buf = malloc(i->rx_bytes * sizeof(uint8_t));
     furi_hal_i2c_acquire(I2C_HANDLE);
     i->i2c_ok = false;
     i->i2c_ok = furi_hal_i2c_trx(
-        I2C_HANDLE, i->address, i->tx, sizeof(i->tx), &rx_buf[0], i->rx_bytes, I2C_TIMEOUT);
+        I2C_HANDLE, (i->address) << 1, i->tx, i->tx_bytes, i->rx, i->rx_bytes, I2C_TIMEOUT);
     furi_hal_i2c_release(I2C_HANDLE);
 }
 
+/**
+ * @brief 
+ * @details
+ * @param i i2c context
+ */
 void i2c_rx(I2CTRx* i) {
     // wipe rx data to prepare for rx
     memset(i->rx, 0, 256);
-    // dynamically allocate array for expected bytes
-    uint8_t* rx_buf = malloc(i->rx_bytes * sizeof(uint8_t));
     furi_hal_i2c_acquire(I2C_HANDLE);
-    i->i2c_ok = furi_hal_i2c_rx(I2C_HANDLE, i->address, &rx_buf[0], i->rx_bytes, I2C_TIMEOUT);
+    i->i2c_ok = furi_hal_i2c_rx(I2C_HANDLE, (i->address) << 1, i->rx, i->rx_bytes, I2C_TIMEOUT);
     furi_hal_i2c_release(I2C_HANDLE);
 }
