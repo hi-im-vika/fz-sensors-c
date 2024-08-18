24aa68f57ed1047e4a35e0b43c7dfcff65b1841e
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-18 01:04:49 -0700

    begin code for dummy sensor reading based on bh1750 code

--- scenes/dummy.c
+++ scenes/dummy.c
@@ -8,3 +8,40 @@
  */
 
 #include "../sensors.h"
+
+void sensors_scene_dummy_on_enter(void* ctx) {
+    // get context
+    SensorsApp* s = ctx;
+
+    // set up list display
+    VariableItemList* l = s->sensor_config;
+    VariableItem* i;
+    variable_item_list_reset(l);
+
+    // specify bytes to send to device
+    i = variable_item_list_add(l, "Payload", 1, NULL, s);
+    variable_item_set_current_value_index(i, 0);
+    variable_item_set_current_value_text(i, "N/A");
+
+    // specify bytes expected from device
+    i = variable_item_list_add(l, "RX Bytes", 1, NULL, s);
+    variable_item_set_current_value_index(i, 0);
+    variable_item_set_current_value_text(i, "N/A");
+
+    // send command
+    i = variable_item_list_add(l, "Send", 0, NULL, s);
+
+    // switch to view after setup
+    view_dispatcher_switch_to_view(s->vd, SensorsAppView_SensorConfig);
+}
+
+bool sensors_scene_dummy_on_event(void* ctx, SceneManagerEvent evt) {
+    UNUSED(ctx);
+    UNUSED(evt);
+    return false;
+}
+
+void sensors_scene_dummy_on_exit(void* ctx) {
+    SensorsApp* s = ctx;
+    variable_item_list_reset(s->sensor_config);
+}
