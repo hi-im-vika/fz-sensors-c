da03502cde10bef6ec8c240014d557e4d3105e6b
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-17 21:31:44 -0700

    separate handler code into source and header

--- gui/handlers.h
+++ gui/handlers.h
@@ -5,3 +5,8 @@
 #include "events/on_enter.h"
 #include "events/on_event.h"
 #include "events/on_exit.h"
+
+extern void (*const scene_on_enter[])(void*);
+extern void (*const scene_on_exit[])(void*);
+extern bool (*const scene_on_event[])(void*, SceneManagerEvent);
+extern const SceneManagerHandlers handlers;
