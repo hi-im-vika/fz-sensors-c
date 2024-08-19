eca9bc3e29bcf2cb27424e951ba1d9b5496fd393
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-19 16:15:08 -0700

    implement code for byte input event handlers

--- scenes/byte_input.c
+++ scenes/byte_input.c
@@ -9,3 +9,70 @@
 
 #include "../sensors_i.h"
 #include "dummy_i.h"
+
+/**
+ * @brief runs when entering the byte input scene
+ * 
+ * @param ctx app context
+ */
+void sensors_scene_byte_input_on_enter(void* ctx) {
+    // get context
+    SensorsApp* s = ctx;
+    if(scene_manager_has_previous_scene(s->sm, SensorsAppScene_DummySensor)) {
+        switch(variable_item_list_get_selected_item_index(s->sensor_config)) {
+            case(DummyVarItemListIndexAddress):
+                byte_input_set_header_text(s->byte_input, "Enter Address");
+                byte_input_set_result_callback(
+                    s->byte_input, byte_text_input_callback, NULL, s, &(s->it->address), 1);
+                break;
+            case(DummyVarItemListIndexPayload):
+                byte_input_set_header_text(s->byte_input, "Enter Payload");
+                byte_input_set_result_callback(
+                    s->byte_input, byte_text_input_callback, NULL, s, (s->it->tx), s->it->tx_bytes);
+                break;
+            default:
+                break;
+        }
+    }
+
+    view_dispatcher_switch_to_view(s->vd, SensorsAppView_ByteInput);
+}
+
+/**
+ * @brief runs when an event is detected in the byte input scene
+ * 
+ * @param ctx app context
+ * @param evt detected event
+ * @return true event was handled
+ * @return false event was not handled
+ */
+bool sensors_scene_byte_input_on_event(void* ctx, SceneManagerEvent evt) {
+    SensorsApp* s = ctx;
+    bool consumed = false;
+    switch(evt.type) {
+        case SceneManagerEventTypeCustom:
+            switch(evt.event) {
+                case SensorsAppEvent_EnterBytes:
+                    scene_manager_next_scene(s->sm, SensorsAppScene_DummySensor);
+                    consumed = true;
+                    break;
+                default:
+                    consumed = false;
+                    break;
+            }
+            break;
+        default: // eg. SceneManagerEventTypeBack, SceneManagerEventTypeTick
+            consumed = false;
+            break;
+    }
+    return consumed;
+}
+
+/**
+ * @brief runs when leaving the byte input scene
+ * 
+ * @param ctx app context
+ */
+void sensors_scene_byte_input_on_exit(void* ctx) {
+    UNUSED(ctx);
+}
