5dab7387009614d5ce4bbc139ea9a17be9c92151
vika <37137412+hi-im-vika@users.noreply.github.com>
2024-08-19 16:11:06 -0700

    set up scenes for byte input and text box

--- scenes/scene_config.h
+++ scenes/scene_config.h
@@ -2,3 +2,5 @@
 ADD_SCENE(sensors, home, Home)
 ADD_SCENE(sensors, dummy, DummySensor)
 ADD_SCENE(sensors, bh1750, BH1750)
+ADD_SCENE(sensors, byte_input, ByteInput)
+ADD_SCENE(sensors, text_box, TextBox)
